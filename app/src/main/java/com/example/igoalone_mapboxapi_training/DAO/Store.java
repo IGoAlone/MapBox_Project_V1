2f404532e76ceab8a30bcb979a9f8924f8c7fa39
Park soo yeon <tndus130@ajou.ac.kr>
2020-04-29 14:42:00 +0900

    add DAO

---
+++ app/src/main/java/com/example/igoalone_mapboxapi_training/DAO/Store.java
@@ -0,0 +1,32 @@
+package com.example.igoalone_mapboxapi_training.DAO;
+
+public class Store {
+    String road_name;
+    float latitude;
+    float longitude;
+
+    public String getRoad_name() {
+        return road_name;
+    }
+
+    public void setRoad_name(String road_name) {
+        this.road_name = road_name;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+}
